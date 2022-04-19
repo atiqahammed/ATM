e264d6752298b1c7b34b67676b7c4303d3279c29
Atiq Ahammed <atiqahammedshamim@gmail.com>
2022-04-20 00:26:25 +0600

    signature based contract update

---
+++ contracts/CreaboFund.sol
@@ -0,0 +1,62 @@
+pragma solidity 0.8.4;
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+
+contract CreaboFund is EIP712, AccessControl  {
+
+    string private constant SIGNING_DOMAIN = "CREABO-Voucher";
+    string private constant SIGNATURE_VERSION = "1";
+
+    struct CREABOVoucher {
+        uint256 projectId;
+        uint256 amount;
+        bytes signature;
+    }
+
+    constructor()
+        EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION){}
+
+    mapping(address => uint) public balances;
+
+    event Deposit(address sender, uint amount);
+    event Withdrawal(address receiver, uint amount);
+    event Transfer(address sender, address receiver, uint amount, uint256 projectId);
+
+    function deposit() public payable {
+        emit Deposit(msg.sender, msg.value);
+        balances[msg.sender] += msg.value;
+    }
+    function _hash(CREABOVoucher calldata voucher) internal view returns (bytes32) {
+        return _hashTypedDataV4(keccak256(abi.encode(
+            keccak256("CREABOVoucher(uint256 projectId,uint256 amount)"),
+            voucher.projectId,
+            voucher.amount
+        )));
+    }
+
+    function _verify(CREABOVoucher calldata voucher) internal view returns (address) {
+        bytes32 digest = _hash(voucher);
+        return ECDSA.recover(digest, voucher.signature);
+    }
+
+    function withdraw(uint amount, CREABOVoucher calldata voucher) public {
+        address signer = _verify(voucher);
+        require(msg.sender == signer, "Signature invalid or unauthorized");
+        require(balances[msg.sender] >= amount, "Insufficient funds");
+        emit Withdrawal(msg.sender, amount);
+        balances[msg.sender] -= amount;
+    }
+
+    function transferFund(address receiver, uint amount, CREABOVoucher calldata voucher) public {
+        address signer = _verify(voucher);
+        require(msg.sender == signer, "Signature invalid or unauthorized");
+        require(balances[msg.sender] >= amount, "Insufficient funds");
+        emit Transfer(msg.sender, receiver, amount, voucher.projectId);
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,109 +1,99 @@
 const { expect } = require("chai");
 const hardhat = require("hardhat");
 const { ethers } = hardhat;
-const { LazyMinter } = require('../lib');
-const EthUtil = require('ethereumjs-util')
-const EthTx = require('ethereumjs-tx').Transaction;
-const ethSigUtil = require("eth-sig-util");
+const { LazyMinter } = require("../lib");
 // const web3 = require("@nomiclabs/hardhat-web3");
 
-describe("ATM", function() {
-  it("Should deploy", async function() {
+describe("CreaboFund", function () {
+  it("Should deploy", async function () {
     const signers = await ethers.getSigners();
-    const minter = signers[0];
-    console.log(minter.address);
+    const signer = signers[0];
+    console.log(signer.address);
 
-    expect(minter).not.null;
-    const ATM = await ethers.getContractFactory("ATM");
-    const atmContract = await ATM.deploy();
-    await atmContract.deployed();
+    expect(signer).not.null;
+    const CreaboFund = await ethers.getContractFactory("CreaboFund");
+    const CreaboFundContract = await CreaboFund.deploy();
+    await CreaboFundContract.deployed();
 
-    const lazyMinter = new LazyMinter({ contractAddress: atmContract.address, signer: minter });
-    const { voucher, signature, digest } = await lazyMinter.createVoucher(1, "ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi")
+    const lazyMinter = new LazyMinter({ contractAddress: CreaboFundContract.address, signer: signer });
+    const voucher = await lazyMinter.createVoucher(1, 10);
 
-    console.log(['signature', signature]);
-    let accounts = await web3.eth.getAccounts();
-    console.log(accounts)
+    // console.log(['signature', signature]);
+    // let accounts = await web3.eth.getAccounts();
+    //   console.log(accounts)
 
+    // const myVoucher = {
+    //   ...voucher,
+    //   signature
+    // }
 
-    let transaction1 = await atmContract.deposit({ from: minter.address, value: 5000 });
-    console.log(transaction1);
-    let transaction2 = await atmContract.withdraw(5000, { from: minter.address });
-    console.log(transaction2);
-    
+    // address = ethers.utils.verifyMessage(digest, signature);
+    //   console.log(address);
 
-    // const tx = new EthTx(signature, {chain: 'hardhat', hardfork: 'petersburg'})
+    let transaction1 = await CreaboFundContract.deposit({ from: signer.address, value: 5000 });
+    // console.log(transaction1);
+    // let transaction2 = await atmContract.withdraw(5000, voucher, { from: minter.address });
+    let transaction2 = await CreaboFundContract.transferFund(signers[1].address, 5000, voucher, { from: signer.address });
+    // console.log(transaction2);
 
-    // let transaction = web3.eth.accounts.recover(voucher, signature);
-    // console.log(['transaction', transaction]);
+    //   // const tx = new EthTx(signature, {chain: 'hardhat', hardfork: 'petersburg'})
 
-    // console.log(atmContract.address);
+    //   // let transaction = web3.eth.accounts.recover(voucher, signature);
+    //   // console.log(['transaction', transaction]);
 
-    const msgParams = {
-      // data: nonce,
-      sig: signature
-  };
-  const output =  ethSigUtil.extractPublicKey(msgParams);
-  console.log(output);
+    //   // console.log(atmContract.address);
 
-  const recoveredPubKey = ethers.utils.recoverPublicKey(
-    digest,
-    signature
-  );
-  const recoveredAddress = ethers.utils.recoverAddress(digest, signature);
-  console.log(recoveredAddress);
-  console.log(recoveredPubKey);
+    //   const msgParams = {
+    //     // data: nonce,
+    //     sig: signature
+    // };
+    // const output =  ethSigUtil.extractPublicKey(msgParams);
+    // console.log(output);
 
-  const message = ethers.utils.solidityKeccak256(
-    ['uint256', 'uint256', 'string'],
-   [voucher.tokenId, voucher.minPrice, ethers.utils.keccak256(ethers.utils.toUtf8Bytes(voucher.uri))]);
-   const arrayified_message = ethers.utils.arrayify(message);
+    // const recoveredPubKey = ethers.utils.recoverPublicKey(
+    //   digest,
+    //   signature
+    // );
+    // const recoveredAddress = ethers.utils.recoverAddress(digest, signature);
+    // console.log(recoveredAddress);
+    // console.log(recoveredPubKey);
 
- 
-   const recoveredAddress2 = ethers.utils.recoverAddress(arrayified_message, signature);
+    // const message = ethers.utils.solidityKeccak256(
+    //   ['uint256', 'uint256', 'string'],
+    //  [voucher.tokenId, voucher.minPrice, ethers.utils.keccak256(ethers.utils.toUtf8Bytes(voucher.uri))]);
+    //  const arrayified_message = ethers.utils.arrayify(message);
 
-   console.log(recoveredAddress2)
+    //  const recoveredAddress2 = ethers.utils.recoverAddress(arrayified_message, signature);
 
+    //  console.log(recoveredAddress2)
 
-   const signThing = "0x50e255a73d200fd6365e4c58f756df5dd7e26ed02bed3b5a9baca066394fba26";
-   const message1 = signThing;
-                    const message2 = ethers.utils.arrayify(message1);
+    //  const signThing = "0x50e255a73d200fd6365e4c58f756df5dd7e26ed02bed3b5a9baca066394fba26";
+    //  const message1 = signThing;
+    //                   const message2 = ethers.utils.arrayify(message1);
 
-    let sig = await minter.signMessage(message2);
-    console.log(sig);
+    //   let sig = await minter.signMessage(message2);
+    //   console.log(sig);
 
-    let address = ethers.utils.recoverAddress(message1, sig);
-    console.log(address);
+    //   let address = ethers.utils.recoverAddress(message1, sig);
+    //   console.log(address);
 
-    console.log('--------------');
+    //   console.log('--------------');
 
-    let hashMessage = ethers.utils.hashMessage("Hello World");
-    // const array = ethers.utils.arrayify(message1);
-    let sign = await minter.signMessage(hashMessage);
-    console.log(sign);
-    address = ethers.utils.verifyMessage(hashMessage, sign);
-    console.log(address);
-    console.log(minter.address);
-    address = ethers.utils.verifyMessage(digest, signature);
-
-    console.log(address);
-
-
-
-
+    //   let hashMessage = ethers.utils.hashMessage("Hello World");
+    //   // const array = ethers.utils.arrayify(message1);
+    //   let sign = await minter.signMessage(hashMessage);
+    //   console.log(sign);
+    //   address = ethers.utils.verifyMessage(hashMessage, sign);
+    //   console.log(address);
+    //   console.log(minter.address);
 
     /*
   // web3.eth.accounts.recover('Some data', '0xb91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c');
   // web3.eth.accounts.recover('Some data', '0xb91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c');
-
     */
     // then( (signed_msg) => {
-    //                     console.log('address: ' + ethers.utils.recoverAddress(message1, signed_msg));
-
-  //uint256 tokenId,uint256 minPrice,string uri utils.keccak256(utils.toUtf8Bytes("example"))
-
-  // web3.eth.accounts.recover('Some data', '0xb91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c');
-
+    //                     console.log('address: ' + ethers.utils.recoverAddress(message1, signed_msg))
+    //uint256 tokenId,uint256 minPrice,string uri utils.keccak256(utils.toUtf8Bytes("example"))
+    // web3.eth.accounts.recover('Some data', '0xb91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c');
   });
-
 });
